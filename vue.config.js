2887aeae2a612895b39aad2d5bec329f3d3e3c0b
GodQ <quchuanhao@gmail.com>
2023-11-05 09:22:00 -0500

    a

--- vue.config.js
+++ vue.config.js
@@ -7,7 +7,7 @@ module.exports = {
         disableHostCheck: true,
         proxy: {
             '/api':{
-                target:'http://10.110.124.130:5000',
+                target:'http://127.0.0.1:5000',
                 changeOrigin:true,
                 timeout: 600000000
                 // pathRewrite:{
